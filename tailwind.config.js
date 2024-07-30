c757680fd7934b521d7215a53a15309ebffe266f
matheuzin <mackingdead@gmail.com>
2024-07-29 21:24:38 -0300

    ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    backdropBlur: {
-      xs: "2px",
-      sm: "4px",
-      md: "8px",
-      lg: "12px",
-    },
-    aspectRatio: {
-      md: "3/2",
-    },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  backdropBlur: {
+    xs: "2px",
+    sm: "4px",
+    md: "8px",
+    lg: "12px",
   },
-  plugins: [],
+  aspectRatio: {
+    md: "3/2",
+  },
 };
+export const plugins = [];
